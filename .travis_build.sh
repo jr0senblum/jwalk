be14009e4c4a0d525c75ee1df136b5f5997d1da8
Rosenblum, Jim <jim.rosenblum@gmail.com>
2019-07-23 13:53:44 -0700

    no message

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget https://s3.amazonaws.com/rebar3/rebar3
+#wget https://s3.amazonaws.com/rebar3/rebar3
+rm -rf rebar3
 git clone https://github.com/erlang/rebar3.git
  cd rebar3
  ./bootstrap
