e465f25760f9b7afbdc5d77c18547c3999c6d815
Rosenblum, Jim <jim.rosenblum@gmail.com>
2019-07-23 13:46:09 -0700

    build rebar as part of travis for different versions of erlang

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-#wget https://s3.amazonaws.com/rebar3/rebar3
+wget https://s3.amazonaws.com/rebar3/rebar3
+it clone https://github.com/erlang/rebar3.git
+ cd rebar3
+ ./bootstrap
+cp rebar3 ../
+cd ..
 
 chmod a+x ./rebar3
 make clean
