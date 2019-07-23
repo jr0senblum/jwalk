102540aae766ffbda00e225a9a459753faab98a6
Rosenblum, Jim <jim.rosenblum@gmail.com>
2019-07-23 14:32:12 -0700

    no message

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-wget https://s3.amazonaws.com/rebar3/rebar3
+#wget https://s3.amazonaws.com/rebar3/rebar3
 
 
-chmod a+x ./rebar3
-make clean
-make compile
-make eunit
+#chmod a+x ./rebar3
+#make clean
+#make compile
+#make eunit
 
